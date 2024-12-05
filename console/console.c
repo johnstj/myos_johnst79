8c06c1cd884b4f335ea7afe33170de77baa41537
johnstj <johnst79@students.rowan.edu>
2024-12-05 02:26:20 -0500

    Homework Part 3 changes

--- console/console.c
+++ console/console.c
@@ -3,7 +3,8 @@
 
 static int terminal_position = 0;
 static char* const VGA_BUFFER = (char*) 0xb8000;
-
+static VGA_Color terminal_font_color = LIGHT_GRAY; // Default font color will be light gray
+static VGA_Color terminal_background_color = BLACK; // Default background color is black
 
 void clear_terminal() {
 	for (int i=0; i < VGA_WIDTH * VGA_HEIGHT; i+=VGA_BYTES_PER_CHARACTER) {
@@ -11,25 +12,47 @@ void clear_terminal() {
 		VGA_BUFFER[i+1] = 0x07;
 	}
 	terminal_position = 0;
-	return;
 }
 
 void print_line(char* str) {
-	print_string(str);
-	print_character('\n');
-
-	return;
+	print_line_with_color(str, terminal_background_color, terminal_font_color);
 }
 
 void print_string(char* str) {
-	for (int i=0; str[i] != '\0'; i++) {
-		print_character(str[i]);
-	}
-
-	return;
+	print_string_with_color(str, terminal_background_color, terminal_font_color);
 }
 
 void print_character(char c) {
+	
+	print_character_with_color(c, terminal_background_color, terminal_font_color);
+}
+
+void set_terminal_font_color(VGA_Color col) {
+     
+	terminal_font_color = col;
+}
+
+void set_terminal_background_color(VGA_Color col) {
+
+	terminal_background_color = col;
+}
+
+void print_line_with_color(char* str, VGA_Color bg_color, VGA_Color font_color) {
+
+	print_string_with_color(str, bg_color, font_color);
+	print_character_with_color('\n', bg_color, font_color);
+}
+
+void print_string_with_color(char* str, VGA_Color bg_color, VGA_Color font_color) {
+
+	for (int i=0; str[i] != '\0'; i++) {
+     		print_character_with_color(str[i], bg_color, font_color);
+	}
+}
+
+void print_character_with_color(char c, VGA_Color bg_color, VGA_Color font_color) {
+	char style_byte = (bg_color << 4) | font_color;
+	
 	switch(c) {
 		case '\n':
 			terminal_position += (VGA_WIDTH - 
@@ -37,12 +60,10 @@ void print_character(char c) {
 			break;
 		default:
 			VGA_BUFFER[terminal_position 
-				* VGA_BYTES_PER_CHARACTER] = c;
+				* VGA_BYTES_PER_CHARACTER + 1] = style_byte;	
 			VGA_BUFFER[terminal_position 
-				* VGA_BYTES_PER_CHARACTER - 1] = 0x07;
+				* VGA_BYTES_PER_CHARACTER] = c;
 			terminal_position += 1;
 			break;
 	}
-	
-	return;
 }
--- kernel.c
+++ kernel.c
@@ -2,11 +2,14 @@
 #include "console.h"
 
 void main() {
-	// Joe Johnston HWP2
+	// Joe Johnston HWP3
 	clear_terminal();
 
-	print_string("HELLO");
-	print_line("WORLD");
-	print_string("TODAY");
+	print_string_with_color("hello", YELLOW, CYAN);
+
+	print_line_with_color("World", MAGENTA, GREEN);
+
+	print_string("Today");
+
 	return;
 }
